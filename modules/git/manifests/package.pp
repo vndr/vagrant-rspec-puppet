e4da10c393e2cf246cb898c0f25fdd2e465722f5
vndr <jv.vndr@gmail.com>
2013-10-26 23:11:02 +0100

    initial commit for git puppet module

---
+++ modules/git/manifests/package.pp
@@ -0,0 +1,47 @@
+# == Class: git::package
+#
+# This class exists to coordinate all software package management related
+# actions, functionality and logical units in a central place.
+#
+#
+# === Parameters
+#
+# This class does not provide any parameters.
+#
+#
+# === Examples
+#
+# This class may be imported by other classes to use its functionality:
+#   class { 'git::package': }
+#
+# It is not intended to be used directly by external resources like node
+# definitions or other modules.
+#
+#
+# === Authors
+#
+# * vndr <mailto:jv.vndr@gmail.com>
+#
+class git::package {
+
+  #### Package management
+
+  # set params: in operation
+  if $git::ensure == 'present' {
+
+    $package_ensure = $git::autoupgrade ? {
+      true  => 'latest',
+      false => 'present',
+    }
+
+  # set params: removal
+  } else {
+    $package_ensure = 'purged'
+  }
+
+  # action
+  package { $git::params::package:
+    ensure => $package_ensure,
+  }
+
+}
